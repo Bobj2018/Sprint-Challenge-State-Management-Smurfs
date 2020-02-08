a94957dccf4dcbe5d7bb114d18621fb3fdb841e2
Joshua Rieth <joshua.m.rieth@gmail.com>
2020-02-07 23:49:13 -0500

    completed mvp

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
